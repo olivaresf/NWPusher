cd5decd10a4af6e7f64a58efee5ccf9117b0f991
Fernando Olivares <fernando.olivares@me.com>
2015-11-25 08:47:33 -0600

    Added a new framework target for Carthage to build.

---
+++ NWPusherFramework/NWPusherFramework.h
@@ -0,0 +1,25 @@
+//
+//  NWPusherFramework.h
+//  NWPusherFramework
+//
+//  Created by Fernando Olivares on 11/25/15.
+//  Copyright © 2015 noodlewerk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NWPusherFramework.
+FOUNDATION_EXPORT double NWPusherFrameworkVersionNumber;
+
+//! Project version string for NWPusherFramework.
+FOUNDATION_EXPORT const unsigned char NWPusherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NWPusherFramework/PublicHeader.h>
+
+#import "NWHub.h"
+#import "NWNotification.h"
+#import "NWPusher.h"
+#import "NWPushFeedback.h"
+#import "NWSecTools.h"
+#import "NWSSLConnection.h"
+#import "NWType.h"
